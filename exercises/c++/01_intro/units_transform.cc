089876827db2287409e704e1ac0629d77f11a2a3
orleny troconis <troconisorlenys@gmail.com>
2020-10-16 20:25:43 +0200

    add Fahrenheit option

--- exercises/c++/01_intro/units_transform.cc
+++ exercises/c++/01_intro/units_transform.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int transf_Kelvin(double& T_C){
+int transf_CelstoKelv(double& T_C){
 
 	double T_K;
 	T_K = T_C + 273.15;
@@ -10,13 +10,29 @@ int transf_Kelvin(double& T_C){
 	return T_K;
 }
 
+int transf_FahretoKelv(double& T_F){
+
+	double T_K;
+	T_K = ((T_F-32)*0.56) + 273.15;
+
+	std::cout << T_F << " Fahrenheit is equivalent to " << T_K << " Kelvin" << std::endl;
+
+	return T_K;
+}
 int main(){
 
-	double T_C;
-	std::cout << "Please, enter the temperature in Celsius\n";
-	std::cin >> T_C;
+	double T;
+	std::string units;
+	std::cout << "Please, enter the temperature with the units in Celsius (C) or Fahrenheit (F))\n";
+	std::cin >> T;
+	std::cin >> units;
 
-	transf_Kelvin(T_C);
+	if(units == "C")
+		transf_CelstoKelv(T);
+	else if(units == "F")
+		transf_FahretoKelv(T);
+	else
+		std::cout << "The units are not Celsius or Fahrenheit, or the value is not correct\n" << std::endl;
 
 
 
